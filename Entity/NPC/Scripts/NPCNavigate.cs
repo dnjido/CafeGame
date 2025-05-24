6507de65d4c26806a4ecf56715d6e9870654e93a
dnjido <55711935+dnjido@users.noreply.github.com>
2025-05-24 17:44:16 +0600

    NPC Animation

---
+++ Entity/NPC/Scripts/NPCNavigate.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCNavigate : MonoBehaviour
+{
+    [SerializeField] private Transform _movePoint;
+
+    private NavMeshAgent _agent => GetComponent<NavMeshAgent>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
